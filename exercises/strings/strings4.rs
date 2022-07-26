15e3f3abb8f8fbc3651a8ce9eace1cc58729a46f
jtabalon <joseph@tabalon.io>
2022-07-26 12:57:52 -0700

    complete happy tuesday

--- exercises/strings/strings4.rs
+++ exercises/strings/strings4.rs
@@ -24,6 +24,6 @@ fn main() {
     string(format!("Interpolation {}", "Station"));
     string_slice(&String::from("abc")[0..1]);
     string_slice("  hello there ".trim());
-    // ???("Happy Monday!".to_string().replace("Mon", "Tues"));
+    string("Happy Monday!".to_string().replace("Mon", "Tues"));
     // ???("mY sHiFt KeY iS sTiCkY".to_lowercase());
 }
