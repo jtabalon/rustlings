641294f617c24533d94c29791b0a605c3ff54792
jtabalon <joseph@tabalon.io>
2022-07-26 12:58:13 -0700

    complete my shift key is sticky

--- exercises/strings/strings4.rs
+++ exercises/strings/strings4.rs
@@ -25,5 +25,5 @@ fn main() {
     string_slice(&String::from("abc")[0..1]);
     string_slice("  hello there ".trim());
     string("Happy Monday!".to_string().replace("Mon", "Tues"));
-    // ???("mY sHiFt KeY iS sTiCkY".to_lowercase());
+    string("mY sHiFt KeY iS sTiCkY".to_lowercase());
 }
