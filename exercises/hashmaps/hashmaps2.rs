a4098d16dc0f9bcd2c8b8c4e097bb9e5ee65b95a
jtabalon <joseph@tabalon.io>
2022-07-27 07:15:35 -0700

    second submission hashmaps2 (or insert)

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -35,9 +35,7 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
 
 
     for fruit in fruit_kinds {
-        if !basket.contains_key(&fruit) {
-            basket.insert(fruit, 5);
-        }
+        basket.entry(fruit).or_insert(5);
         // TODO: Put new fruits if not already present. Note that you
         // are not allowed to put any type of fruit that's already
         // present!
